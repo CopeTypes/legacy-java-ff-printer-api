56488f9299422be117ff45c59ee243ae889edd2e
CopeTypes <106415648+CopeTypes@users.noreply.github.com>
2023-11-22 21:46:44 -0500

    Clean up PrintMonitor
    Moved all the checks to a single thread

--- src/main/java/me/ghost/printmonitor/PrintMonitor.java
+++ src/main/java/me/ghost/printmonitor/PrintMonitor.java
@@ -29,6 +29,7 @@ public class PrintMonitor {
     private WebhookUtil webhook;
     private PrinterClient client;
     private PrinterWebcam webcam;
+    private ThermalSafety thermalSafety;
 
     //private TempInfo tempInfo;
     //private PrintStatus printStatus;
@@ -43,84 +44,116 @@ public class PrintMonitor {
 
     private int checkFails = 0;
 
-    private final ExecutorService executor = Executors.newCachedThreadPool();
+    SystemTimer lastSync = new SystemTimer();
+    SystemTimer lastDiscordSync = new SystemTimer();
+    SystemTimer lastTempCheck = new SystemTimer();
+    SystemTimer lastFailCheck = new SystemTimer();
+
+    private final ExecutorService thread = Executors.newSingleThreadExecutor();
 
     public PrintMonitor(String printerIp, String webhookUrl) throws PrinterException, InterruptedException {
-        //this.printerIp = printerIp;
         webhook = new WebhookUtil(webhookUrl);
         client = new PrinterClient(printerIp);
         webcam = new PrinterWebcam(printerIp);
+        thermalSafety = new ThermalSafety(client, webhookUrl);
+
+        long t = System.currentTimeMillis() - 30000;
+        lastSync.setTime(t);
+        lastDiscordSync.setTime(t);
+        lastTempCheck.setTime(t);
+        lastFailCheck.setTime(t);
+
         init();
     }
 
     public void start() throws PrinterException, InterruptedException {
-        sync();
+        doSync();
         if (!isPrinting) {
             Logger.log("Waiting for print job to start...");
             waitForPrintJob();
         }
-        //webhook.sendMessage("Monitoring " + printerInfo.getName(), "Job: " + getJobName(), EmbedColors.BLUE);
         sendImageToWebhook("Monitoring " + printerInfo.getName(), "Job: " + getJobName(), EmbedColors.BLUE);
         PrintMonitorApi.refreshUUIDs();
         sleep(500);
-        executor.execute(this::safetyThread);
-        executor.execute(this::detectionThread);
-        executor.execute(this::discordThread);
-        executor.execute(this::syncThread);
-        //Runtime.getRuntime().addShutdownHook(new Thread(executor::shutdown));
+        thread.execute(this::checkThread);
     }
 
-    /**
-     * Handles defect/failure detection and auto-cancels print if necessary
-     */
-    private void detectionThread() {
-        Logger.log("Failure detection thread started");
+    private void checkThread() {
         while (isPrinting) {
+            if (lastFailCheck.hasPassed(5000)) { // Check for print defects/failure every 5sec
+                lastFailCheck.reset();
+                defectCheck();
+            }
+            if (lastSync.hasPassed(5000)) { // Sync necessary monitoring info with the printer every 5sec
+                lastFailCheck.reset();
+                doSync();
+            }
+            if (!isPrinting) break;
+            if (lastTempCheck.hasPassed(10000)) { // Thermal runaway failsafe check every 10sec
+                lastTempCheck.reset();
+                if (!thermalSafety.safe) {
+                    failureShutdown();
+                    System.exit(-1);
+                }
+            }
+            if (lastDiscordSync.hasPassed(30000)) { // Sync job info & picture with discord webhook every 5 minutes
+                lastDiscordSync.reset();
+                sendDiscordReport();
+            }
+            sleep(1000);
+        }
+        Logger.log("Print completed, sending notification to discord and quitting.");
+        sendImageToWebhook("Print complete!", "Your print has finished!", EmbedColors.GREEN);
+        //System.exit(0);
+    }
+
+    private void defectCheck() {
+        Logger.log("Checking the current print for defects");
+        try {
+            DefectStatus status = PrintMonitorApi.getDefectStatus();
+            if (status == null) {
+                checkFails++; // failsafe triggered after 5 failed defect checks in a row
+                if (checkFails >= 5) {
+                    webhook.sendMessage("Detection Failure", "There have been multiple failed defect checks in a short period of time, aborting the print.", EmbedColors.RED);
+                    failureShutdown();
+                }
+                return;
+            }
+            if (checkFails > 0) checkFails--;
+            if (status.defect) {
+                if (!sendImageToWebhook("Print Defect Detected", "Defect detected in the last print check", EmbedColors.RED)) {
+                    Logger.error("Defect detected, unable to get webcam capture.");
+                    webhook.sendMessage("Print Defect Detected", "There was a defect detected in the last print check, but capturing the webcam failed.", EmbedColors.RED);
+                }
+                if (status.score >= 0.6F) {
+                    if (!sendImageToWebhook("Print Failure Detected", "The current print has failed, aborting now", EmbedColors.RED)) {
+                        Logger.error("Failure detected, unable to get webcam capture.");
+                        webhook.sendMessage("Print Failure Detected", "Aborting print now (unable to capture webcam)", EmbedColors.RED);
+                    }
+                    failureShutdown();
+                }
+            }
+        } catch (IOException e) {
+            Logger.error("Failed to check print defect status: " + e.getMessage());
+        }
+    }
+
+    private void doSync() {
+        try {
+            isPrinting = client.isPrinting();
+            //sync()
+        } catch (PrinterException e) {
+            Logger.error("Sync failure: " + e.getMessage());
             sleep(5000);
             try {
-                Logger.log("Checking DefectStatus");
-                DefectStatus status = PrintMonitorApi.getDefectStatus();
-                if (status == null) {
-                    checkFails++; // failsafe triggered after 5 failed defect checks in a row
-                    if (checkFails >= 5) {
-                        webhook.sendMessage("Detection Failure", "There have been multiple failed defect checks in a short period of time, aborting the print.", EmbedColors.RED);
-                        failureShutdown();
-                    }
-                    return;
-                }
-                if (checkFails > 0) checkFails--;
-                if (status.defect) {
-                    if (!sendImageToWebhook("Print Defect Detected", "Defect detected in the last print check", EmbedColors.RED)) {
-                        Logger.error("Defect detected, unable to get webcam capture.");
-                        webhook.sendMessage("Print Defect Detected", "There was a defect detected in the last print check, but capturing the webcam failed.", EmbedColors.RED);
-                    }
-                    if (status.score >= 0.6F) {
-                        if (!sendImageToWebhook("Print Failure Detected", "The current print has failed, aborting now", EmbedColors.RED)) {
-                            Logger.error("Failure detected, unable to get webcam capture.");
-                            webhook.sendMessage("Print Failure Detected", "Aborting print now (unable to capture webcam)", EmbedColors.RED);
-                        }
-                        failureShutdown();
-                        break;
-                    }
-                }
-            } catch (IOException e) {
-                Logger.error("Failed to check print defect status: " + e.getMessage());
+                isPrinting = client.isPrinting();
+                //sync()
+            } catch (Exception ignored) {
+                Logger.log("Sync lost with printer, aborting current job for safety.");
+                webhook.sendMessage("Sync failed", "Unable to sync with printer, attempting to abort current job.", EmbedColors.RED);
+                shutdownFailsafe();
             }
         }
-    }
-
-    /**
-     * Handles sending status updates with an image to Discord every 5 minutes
-     */
-    private void discordThread() {
-        Logger.log("Discord thread started");
-        sendDiscordReport();
-        while (isPrinting) {
-            sleep(300000); // every 5 minutes
-            if (commandLock || syncing) sleep(5000);
-            sendDiscordReport();
-        }
-        //sendImageToWebhook("Print Complete!", "Your print has finished!", EmbedColors.GREEN);
     }
 
     private void sendDiscordReport() {
@@ -134,49 +167,6 @@ public class PrintMonitor {
             if (!NetworkUtil.sendPrintReport(webhook.getUrl(), report, new File(out))) Logger.error("Failed to send print report to discord webhook.");
         } catch (PrinterException e) {
             Logger.error("Error getting print report for sendDiscordReport: " + e);
-        }
-    }
-
-    /**
-     * Handles syncing the printer data every 5 seconds
-     */
-    private void syncThread() {
-        Logger.log("Sync thread started");
-        while (isPrinting) {
-            sleep(5000);
-            try {
-                sync();
-            } catch (PrinterException e) {
-                Logger.error("Sync failure: " + e.getMessage());
-                sleep(5000);
-                try {
-                    sync();
-                } catch (Exception ignored) {
-                    Logger.log("Sync lost with printer, aborting current job for safety.");
-                    webhook.sendMessage("Sync failed", "Unable to sync with printer, attempting to abort current job.", EmbedColors.RED);
-                    shutdownFailsafe();
-                }
-            } catch (InterruptedException ignored) {}
-        }
-        sendImageToWebhook("Print Complete!", "Your print has finished!", EmbedColors.GREEN);
-        System.exit(0);
-    }
-
-    /**
-     * Handles the safety checks and auto-cancels print if one fails
-     */
-    private void safetyThread() {
-        Logger.log("Safety thread started");
-        ThermalSafety thermalSafety = new ThermalSafety(client, webhook.getUrl());
-        while (isPrinting) {
-            sleep(5000);
-            if (syncing) sleep(1500);
-            try {
-                thermalSafety.run();
-            } catch (PrinterException e) {
-                Logger.error("ThermalSafety run failure: " + e.getMessage());
-                if (!thermalSafety.safe) failureShutdown();
-            }
         }
     }
 
@@ -239,24 +229,15 @@ public class PrintMonitor {
         commandLock = false;
     }
 
-    private void sync() throws PrinterException, InterruptedException {
+    /*private void sync() throws PrinterException, InterruptedException {
         syncing = true;
         commandLock = true;
-        //tempInfo = client.getTempInfo();
-        //Thread.sleep(500);
-        //printStatus = client.getPrintStatus();
-        //Thread.sleep(500);
-        //endstopStatus = client.getEndstopStatus();
-        //Thread.sleep(500);
-        //machineStatus = client.getMachineStatus();
-        //Thread.sleep(500);
-        //moveStatus = client.getMoveStatus();
         Thread.sleep(500);
         isPrinting = client.isPrinting();
         Thread.sleep(500);
         commandLock = false;
         syncing = false;
-    }
+    }*/
 
     private void waitForPrintJob() throws PrinterException {
         commandLock = true;
